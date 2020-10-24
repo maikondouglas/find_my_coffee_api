74e903c6a16740a77b71e8574b1b15be7111d38b
Maikon Douglas <maikonconta0@gmail.com>
2020-10-24 01:41:36 -0300

    Created controller to in stores at config initial

---
+++ app/controllers/api/v1/stores_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::StoresController < ApplicationController
+  before_action :set_store, only: %i[show]
+
+  def index
+    return [] if params[:latitude].nil? ^ params[:longitude].nil?
+
+    @stores = Store.within(params[:latitude].to_f, params[:latitude].to_f)
+                   .sort_by(&:ratings_average).reverse
+  end
+
+  def show; end
+
+  private
+
+  def set_store
+    @store = Store.find_by!(google_place_id: params[:id])
+  end
+end
