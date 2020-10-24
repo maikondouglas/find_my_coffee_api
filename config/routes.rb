5c70c3afc29e98d32cd94729fb1da93b61dbc805
Maikon Douglas <maikonconta0@gmail.com>
2020-10-24 17:59:02 -0300

    Created to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :stores, only: %i[index show], defaults: { format: 'json' }
+      resources :ratings, only: %i[create], defaults: { format: 'json' }
+      resources :google_stores, only: %i[index show], defaults: { format: 'json' }
     end
   end
 end
